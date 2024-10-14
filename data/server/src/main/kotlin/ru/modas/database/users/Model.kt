427542d8548e0ffd0f414ec58e07c73f64602fdd
morphine198 <terpilx9@gmail.com>
2024-10-14 19:30:47 +0300

    added functionality to 'users' model

--- data/server/src/main/kotlin/ru/modas/database/users/Model.kt
+++ data/server/src/main/kotlin/ru/modas/database/users/Model.kt
@@ -1,9 +1,29 @@
 package ru.modas.database.users
 
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object Model: Table("users") {
-    private val login = Model.varchar("login", 32);
-    private val password = Model.varchar("password", 256);
-    private val email = Model.varchar("email", 128);
+    private val login = Model.varchar("login", 32)
+    private val password = Model.varchar("password", 256)
+    private val email = Model.varchar("email", 128)
+
+    fun insert(dto: DTO) {
+        transaction {
+            Model.insert {
+                it[login] = dto.login
+                it[password] = dto.password
+                it[email] = dto.email
+            }
+        }
+    }
+
+    fun fetch(login: String): DTO {
+        val model = Model.selectAll().where { Model.login.eq(login) }.single()
+        return DTO(
+            login = model[Model.login],
+            password = model[Model.password],
+            email = model[Model.email],
+        )
+    }
 }
