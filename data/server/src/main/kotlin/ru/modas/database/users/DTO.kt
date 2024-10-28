6b7726782a7ae75ee68295841e47f40fc6c333eb
morphine198 <terpilx9@gmail.com>
2024-10-28 17:11:33 +0300

    made 'users' more suitable with existing db

--- data/server/src/main/kotlin/ru/modas/database/users/DTO.kt
+++ data/server/src/main/kotlin/ru/modas/database/users/DTO.kt
@@ -1,6 +1,7 @@
 package ru.modas.database.users
 
 class DTO (
+    val id_user: Int,
     val login: String,
     val password: String,
     val email: String,
--- data/server/src/main/kotlin/ru/modas/database/users/Model.kt
+++ data/server/src/main/kotlin/ru/modas/database/users/Model.kt
@@ -4,6 +4,7 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object Model: Table("users") {
+    private val id_user = Model.integer("id_user")
     private val login = Model.varchar("login", 32)
     private val password = Model.varchar("password", 256)
     private val email = Model.varchar("email", 128)
@@ -18,9 +19,10 @@ object Model: Table("users") {
         }
     }
 
-    fun fetch(login: String): DTO {
-        val model = Model.selectAll().where { Model.login.eq(login) }.single()
+    fun fetch(id_user: Int): DTO {
+        val model = Model.selectAll().where { Model.id_user.eq(id_user) }.single()
         return DTO(
+            id_user = model[Model.id_user],
             login = model[Model.login],
             password = model[Model.password],
             email = model[Model.email],
