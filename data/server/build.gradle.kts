502758b861071cda4622b6dd80637c1a662f2f4b
morphine198 <terpilx9@gmail.com>
2024-11-04 20:39:46 +0300

    added new postgresql dependencies for gradle

--- data/server/build.gradle.kts
+++ data/server/build.gradle.kts
@@ -3,6 +3,7 @@ val kotlin_version: String by project
 val ktor_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
+val postgresql_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.20"
@@ -33,6 +34,8 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.postgresql:postgresql:$postgresql_version")
+
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
