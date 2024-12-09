2b8bb41e9cb029b76e7b064a43d2dceb21b4eee9
morphine198 <144669197+morphine198@users.noreply.github.com>
2024-12-09 16:42:04 +0300

    added token insertion functionality

--- data/server/src/main/kotlin/ru/modas/features/login/Controller.kt
+++ data/server/src/main/kotlin/ru/modas/features/login/Controller.kt
@@ -4,7 +4,7 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import ru.modas.database.users.Model
+import ru.modas.database.users.DTO
 import ru.modas.features.register.DataResponse
 import java.util.*
 
@@ -12,10 +12,19 @@ class Controller (private val call: ApplicationCall) {
     suspend fun loginUser () {
         val receive = call.receive<DataReceive>()
 
-        val userDTO = Model.fetch(receive.login)
+        val userDTO = ru.modas.database.users.Model.fetch(receive.login)
         if (userDTO != null) {
+            val token = UUID.randomUUID().toString()
+
             // Отправка токена
-            call.respond(DataResponse(token = UUID.randomUUID().toString()))
+            call.respond(DataResponse(token = token))
+
+            // Вставка токена
+            ru.modas.database.sessions.Model.insert(
+                ru.modas.database.sessions.DTO(
+                    token = token,
+                )
+            )
             return
         }
 
