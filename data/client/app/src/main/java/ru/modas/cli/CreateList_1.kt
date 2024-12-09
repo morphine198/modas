b57ad921e04ff45192ac8440a2be5edf61986052
Shellix1277 <pavlutin.ivan20001@gmail.com>
2024-12-09 16:13:19 +0300

    List_UI

---
+++ data/client/app/src/main/java/ru/modas/cli/CreateList_1.kt
@@ -0,0 +1,20 @@
+package ru.modas.cli
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+class CreateList_1 : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.activity_create_list1, container, false)
+    }
+}
